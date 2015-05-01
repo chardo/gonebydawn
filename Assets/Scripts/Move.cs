94d8043332c8d12610243dfcaa2615852d240881
Richard Herndon <richardmherndon@gmail.com>
2015-05-01 12:05:53 -0500

    Move player with velociy instead of position

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
-
+public class Move : MonoBehaviour{
+	// Normal Movements Variables
 	public float normalSpeed;
 	public float sneakSpeed;
-	float speed;
-
-	// Use this for initialization
-	void Start () {
-		
+	private Rigidbody2D rb;
+	private Vector2 move;
+	
+	void Start()
+	{	
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift) || Input.GetKey (KeyCode.CapsLock))
-			speed = sneakSpeed;
-		else 
-			speed = normalSpeed;
-
-		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0f);
-		transform.position += move * speed * Time.deltaTime;
+	void FixedUpdate()
+	{
+		move = new Vector2( Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		move.Normalize ();
+		// Move senteces
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			rb.velocity = new Vector2 (Mathf.Lerp (0, Input.GetAxis ("Horizontal") * sneakSpeed, 0.2f),
+		                                  		Mathf.Lerp (0, Input.GetAxis ("Vertical") * sneakSpeed, 0.2f));
+		} else {
+			rb.velocity = new Vector2 (Mathf.Lerp (0, Input.GetAxis ("Horizontal") * normalSpeed, 0.2f),
+			                                    Mathf.Lerp (0, Input.GetAxis ("Vertical") * normalSpeed, 0.2f));
+		}
 	}
 }
