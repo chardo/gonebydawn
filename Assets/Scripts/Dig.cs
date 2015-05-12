ca3b602e3bb1f095483955ec6f223e0eba8f9eb8
Richard Herndon <richardmherndon@gmail.com>
2015-05-12 10:39:24 -0500

    make movement better, add sound trigger, smooth camera zoom

--- Assets/Scripts/Dig.cs
+++ Assets/Scripts/Dig.cs
@@ -7,14 +7,27 @@ public class Dig : MonoBehaviour {
 	private GraveController gc;
 	public float digSpeed;
 
+	//sound radius vars for digging noise
+	public CircleCollider2D soundTrigger;
+	public float digSoundRadius;
+	private float startTime;
+
 
 	// Update is called once per frame
 	void Update () {
+
+		//reset starttime when space is pressed or released (makes Lerp for soundRadius work!)
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyUp (KeyCode.Space)) {
+			startTime = Time.time;
+		}
 
 		if (Input.GetKey (KeyCode.Space)) {
 			if (inGrave && gc.hasLoot) {
 				gc.dirtcount -= digSpeed;
 			}
+			//enable sound radius and set its size to digging radius
+			soundTrigger.enabled = true;
+			soundTrigger.radius = Mathf.Lerp (soundTrigger.radius, digSoundRadius, 8*(Time.time-startTime));
 		}
 	}
 
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,25 +2,56 @@
 using System.Collections;
 
 public class Move : MonoBehaviour{
-	// Normal Movements Variables
+	//speed vars
 	public float normalSpeed;
 	public float sneakSpeed;
+	private float speed;
+	//camera vars
+	public Camera cam;
+	public float normalCamSize;
+	public float sneakCamSize;
+	//movement vars
 	private Rigidbody2D rb;
 	private Vector2 move;
+	//reusable time var for lerping
+	private float startTime;
+	//sound trigger vars
+	public CircleCollider2D soundTrigger;
+	public float normalSoundRadius;
+	public float sneakSoundRadius;
 	
+
 	void Start()
 	{	
 		rb = GetComponent<Rigidbody2D> ();
+		cam.orthographicSize = normalCamSize;
+		soundTrigger.radius = normalSoundRadius;
 	}
-	
-	void FixedUpdate()
+
+	void Update()
 	{
-		move = new Vector2( Mathf.Lerp(0, Input.GetAxis ("Horizontal"), 0.8f), Mathf.Lerp (0, Input.GetAxis ("Vertical"), 0.8f));
-		move.Normalize ();
-		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift) || Input.GetKey (KeyCode.CapsLock)) {
-			rb.velocity = move * sneakSpeed;
-		} else {
-			rb.velocity = move * normalSpeed;
+		//reset startTime on keyDown or keyUp (makes Lerp work!)
+		if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift)
+		    || Input.GetKeyUp (KeyCode.LeftShift) || Input.GetKeyUp (KeyCode.RightShift)) {
+			startTime = Time.time;
 		}
+		//change movement speed, resize camera, change sound radius
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
+			speed = Mathf.Lerp (speed, sneakSpeed, 8*(Time.time-startTime));
+			cam.orthographicSize = Mathf.Lerp (cam.orthographicSize, sneakCamSize, 4*(Time.time-startTime));
+			soundTrigger.radius = Mathf.Lerp (soundTrigger.radius, sneakSoundRadius, 8*(Time.time-startTime));
+		} else {
+			speed = Mathf.Lerp (speed, normalSpeed, 8*(Time.time-startTime));
+			cam.orthographicSize = Mathf.Lerp (cam.orthographicSize, normalCamSize, 4*(Time.time-startTime));
+			soundTrigger.radius = Mathf.Lerp (soundTrigger.radius, normalSoundRadius, 8*(Time.time-startTime));
+		}
+		if (move == Vector2.zero)
+			soundTrigger.enabled = false;
+		else
+			soundTrigger.enabled = true;
+
+		move = new Vector2(Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		move.Normalize ();
+		rb.velocity = move * speed;
 	}
 }
