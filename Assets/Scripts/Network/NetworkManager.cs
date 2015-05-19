eb8ac1f2f486ef8c88652ef03054a4538bd75b6e
kmads <kamaddio@gmail.com>
2015-05-18 19:06:43 -0500

    Added in multiple spawn points
    Multiple spawn points + random assignment to a point upon joining the
    network.

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -1,25 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class NetworkManager : MonoBehaviour {
 	const string VERSION = "v0.0.1";
 	public string roomName = "VVR";
 	public string playerPrefabName = "Digger";
-	public Transform spawnPoint;
-
+	
+	GameObject[] spawnPoints;
+	List<GameObject> spawnOptions;
+	
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings(VERSION);
+		spawnPoints = GameObject.FindGameObjectsWithTag("spawn");
+		spawnOptions = new List<GameObject> ();
+		for (int i = 0; i < spawnPoints.Length; i++) {
+			spawnOptions.Add (spawnPoints [i]);
+		}
 	}
-
+	
 	void OnJoinedLobby() {
 		RoomOptions roomOptions = new RoomOptions (){ isVisible = false, maxPlayers = 4};
 		PhotonNetwork.JoinOrCreateRoom (roomName, roomOptions, TypedLobby.Default);
 	}
-
+	
 	void OnJoinedRoom() {
+		int r = Random.Range (0, spawnOptions.Count);
+		GameObject mySpawnPoint = spawnOptions [r];
+		spawnOptions.Remove(mySpawnPoint);
+		
+		//Debug.Log (spawnOptions.Count);
+		
 		PhotonNetwork.Instantiate (playerPrefabName,
-		                          spawnPoint.position,
-		                          spawnPoint.rotation,
-		                          0);
+		                           mySpawnPoint.transform.position,
+		                           mySpawnPoint.transform.rotation,
+		                           0);
 	}
 }
