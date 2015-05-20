85e9307d16b55730538db15c48469c4195dc2750
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-05-20 18:47:42 -0500

    Guards now investigate sound triggers

--- Assets/Scripts/ChaseTester.cs
+++ Assets/Scripts/ChaseTester.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 using PigeonCoopToolkit.Navmesh2D;
 
 public class ChaseTester : MonoBehaviour {
-
-	public Transform pathingTarget;
-	private List<Vector2> path;
+	
+	public float patrolSpeed;
+	public float chaseSpeed;
+	//
+	public float pathUpdateTime;
 	private float newTargetTimer;
+
+	private bool newTarget;
+
+	// pathing variables
+	private Transform pathingTarget;
+	private List<Vector2> path;
 
 	void Start () {
 		newTargetTimer = 0;
+		newTarget = true;
 	}
 
 	// Update is called once per frame
@@ -18,33 +27,40 @@ public class ChaseTester : MonoBehaviour {
 		if (newTargetTimer > 0)
 			newTargetTimer--;
 
-		if (newTargetTimer == 0 && pathingTarget != null) {
-			path = null;
+		if ( pathingTarget != null && newTargetTimer == 0 && path == null) {
 			path = NavMesh2D.GetSmoothedPath (transform.position, pathingTarget.position);
-			newTargetTimer = 10;
+			newTargetTimer = pathUpdateTime;
+			pathingTarget = null;
 		}
 
 		if(path != null && path.Count != 0)
 		{
-			transform.position = Vector2.MoveTowards(transform.position, path[0], 20*Time.deltaTime);
+			transform.position = Vector2.MoveTowards(transform.position, path[0], chaseSpeed*Time.deltaTime);
 			if(Vector2.Distance(transform.position,path[0]) < 0.01f)
 			{
 				path.RemoveAt(0);
+			}
+			if (path.Count == 0) {
+				path = null;
+				newTarget = true;
 			}
 		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.tag == "Player") {
+		//if player sound bubble runs into the guard
+		if (other.tag == "Player" && newTarget) {
 			pathingTarget = other.transform;
+			newTarget = false;
 		}
 	}
 	
-	void OnTriggerExit2D (Collider2D other) {
+	/*void OnTriggerExit2D (Collider2D other) {
 		if (other.tag == "Player") {
 			pathingTarget = null;
+			Debug.Log ("Player left");
 		}
-	}
+	}*/
 }
 /*
  * 
