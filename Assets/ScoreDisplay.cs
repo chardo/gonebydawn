3940f166b93d334ce9928edd0ded9086dd1a8d98
kmads <kamaddio@gmail.com>
2015-05-21 18:48:43 -0500

    Basic Score UI - Loot count and Rock count

---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+	Text text;
+	public static int currentscore;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		currentscore = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Loot: " + currentscore;
+	}
+}
