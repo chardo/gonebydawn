9ef0aed85317968b467fa3e8916ca51764f9f8a5
kmads <kamaddio@gmail.com>
2015-05-22 09:59:22 -0500

    Added simple DateTime timer to UI
    The timer starts at 1:00 am and increments a minute about every second.

---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Globalization;
+using UnityEngine.UI;
+
+public class TimeDisplay : MonoBehaviour {
+
+	Text timeText;
+	DateTime timeCounter = new DateTime(2011, 6, 10, 01, 00, 00);
+
+	// Use this for initialization
+	void Start () {
+		timeText = GetComponent<Text> ();
+		timeText.text = timeCounter.ToString ("H:mm") + " a.m.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter = timeCounter.AddSeconds (1);
+		timeText.text = timeCounter.ToString ("H:mm") + " a.m.";
+	}
+}
