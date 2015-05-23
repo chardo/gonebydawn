543e5747c0f65d5d2534158327cb7bbd6e7f23f2
Richard Herndon <richardmherndon@gmail.com>
2015-05-23 18:24:53 -0500

    rocks are perfect

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -10,30 +10,14 @@ public class RockController : MonoBehaviour {
 	private SpriteRenderer sprite;
 	private Vector3 startPoint;
 
-//	public Rigidbody2D rock;
-//	public float throwForce;
-//	private Vector2 dir;
-//	private Vector3 startPos;
-//	private float angle;
-//	private PlayerStats ps;
-//	private PhotonView pv;
-
 	// Use this for initialization
 	void Start () {
 		thisCollider = GetComponent<CircleCollider2D> ();
 		rb = GetComponent<Rigidbody2D> ();
 		sprite = GetComponent<SpriteRenderer> ();
 		startPoint = transform.position;
-		
-//		ps = GetComponent<PlayerStats> ();
-//		pv = PhotonView.Get (this);
-	}
 
-//	[RPC]
-//	void UpdateRocks(Vector3 position, Quaternion rotation){
-//		gameObject.transform.position = position;
-//		gameObject.transform.rotation = rotation;
-//	}
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -41,8 +25,6 @@ public class RockController : MonoBehaviour {
 		if (dist > throwDistance) {
 			makeNoiseAndDie();
 		}
-//		pv.RPC ("UpdateRocks", PhotonTargets.AllBuffered, transform.position, transform.rotation);
-
 	}
 
 	void OnCollisionEnter2D (Collision2D other) {
--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -22,7 +22,6 @@ public class Throw : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0) && ps.rocks > 0) {
-//			pv.RPC ("UpdateRocks", PhotonTargets.AllBuffered, r.transform.position, r.transform.rotation);
 			Vector3 mousePos = Input.mousePosition;
 			mousePos.z = (transform.position.z - Camera.main.transform.position.z);
 			mousePos = Camera.main.ScreenToWorldPoint (mousePos);
@@ -31,18 +30,12 @@ public class Throw : MonoBehaviour {
 
 			pv.RPC("ThrowRock", PhotonTargets.AllBuffered, transform.position+d, transform.rotation, dir);
 
-//			angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
-//			GameObject g = PhotonNetwork.Instantiate ("rock", transform.position + d, transform.rotation, 0);
-//			Rigidbody2D r = g.GetComponent<Rigidbody2D>();
-//			r.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-//			r.AddForce (r.transform.right * throwForce);
 			ps.rocks -= 1;
-//			pv.RPC ("UpdateRocks", PhotonTargets.AllBuffered, r.transform.position, r.transform.rotation);
 		}
 	}
 
 	[RPC]
-	void ThrowRock(Vector3 pos, Quaternion rot, Vector3 dir) {
+	void ThrowRock(Vector3 pos, Quaternion rot, Vector2 dir) {
 		float angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
 		Rigidbody2D r = Instantiate (rock, pos, rot) as Rigidbody2D;
 		r.transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
