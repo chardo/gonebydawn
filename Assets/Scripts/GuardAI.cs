1314cde9d7a8eef65ea301e2dbeb0f1df7b2c589
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-05-24 18:46:16 -0500

    new awesome patrolling

--- Assets/Scripts/GuardAI.cs
+++ Assets/Scripts/GuardAI.cs
@@ -30,56 +30,79 @@ public class GuardAI : MonoBehaviour {
 	// patrolling
 	public float waitForPatrol = 2f;
 	private GameObject[] waypoints;
-	private int numWaypoints;
-	private int curWaypoint;
 	private bool waitToPatrol;
+	private Transform prevWaypoint;
+	private Transform curWaypoint;
+	private List<Transform> adjoiningWaypoints;
 
 
 	void Start () {
 		newTargetTimer = 0;
 		sightAngle = normalAngle;
 		waypoints = GameObject.FindGameObjectsWithTag("waypoint");
-		numWaypoints = waypoints.Length;
-		Debug.Log (numWaypoints);
 		currentSpeed = patrolSpeed;
 		waitToPatrol = false;
+		pathingTarget = FindClosestWaypoint();
+		SetNewTarget();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// Count down timer until new target can be set
 		if (newTargetTimer > 0)
 			newTargetTimer--;
 
+		// Pick the next patrol point
 		if ((path == null || path.Count == 0) && !waitToPatrol) {
-			curWaypoint = Random.Range (0, numWaypoints);
-			pathingTarget = waypoints[curWaypoint].transform;
+			WaypointHandler otherScript = curWaypoint.GetComponent<WaypointHandler>();
+			List<Transform> getAdjoining = otherScript.RequestAdjoining();
+
+			adjoiningWaypoints = new List<Transform>();
+			foreach (Transform t in getAdjoining){
+				if (t != prevWaypoint)
+					adjoiningWaypoints.Add (t);
+			}
+
+			int numAdjoining = adjoiningWaypoints.Count;
+			int randWaypoint = Random.Range (0, numAdjoining);
+			pathingTarget = adjoiningWaypoints[randWaypoint];
+
+			prevWaypoint = curWaypoint;
+			curWaypoint = pathingTarget;
 			currentSpeed = patrolSpeed;
+
 			SetNewTarget();
 		}
 
+		// If there is currently a target
 		if(path != null && path.Count != 0)
 		{
+			// move along the path to target
 			transform.position = Vector2.MoveTowards(transform.position, path[0], currentSpeed*Time.deltaTime);
-
 			if(Vector2.Distance(transform.position,path[0]) < 0.01f)
 			{
 				path.RemoveAt(0);
 			}
 
+			// rotate to face direction of travel
 			if (path.Count != 0){
 				Vector3 path3D = new Vector3(path[0].x, path[0].y, transform.position.z);
 				Quaternion rotation = Quaternion.LookRotation
 					(path3D - transform.position, transform.TransformDirection(Vector3.forward));
 				transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
 			}
-			else {
+			// when the target is reached
+			else { 
 				sightAngle = cornerAngle; // Increasing sight angle for 1 frame to look around corners when target is lost
+				// wait waitForPatrol seconds before patrolling
 				if (waitToPatrol){
 					StartCoroutine(WaitForPeriod(waitForPatrol));
 				}
 			}
 		}
 
+		// raycasting for sight; sets target if the player is detected
+		// rays strike walls & player, sets target only if player was hit first
 		for (int i = -sightAngle; i <= sightAngle; i += 5) {
 			Vector2 dir = Quaternion.AngleAxis(i, Vector3.forward) * -transform.up;
 			objectSighted = Physics2D.Raycast (transform.position, dir, 50f, playerMask);
@@ -88,13 +111,15 @@ public class GuardAI : MonoBehaviour {
 					pathingTarget = objectSighted.transform;
 					currentSpeed = chaseSpeed;
 					waitToPatrol = true;
+					SetNewTarget();
 				}
 			}
 		}
+
 		sightAngle = normalAngle;
-		SetNewTarget ();
 	}
 
+	// this function gets the path to a new target and starts the new target timer
 	void SetNewTarget() {
 		if ( pathingTarget != null && newTargetTimer == 0) {
 			path = NavMesh2D.GetSmoothedPath (transform.position, pathingTarget.position);
@@ -112,17 +137,45 @@ public class GuardAI : MonoBehaviour {
 		}
 	}
 
+	// finds and returns the closest waypoint in absolute distance (not accounting for path)
+	Transform FindClosestWaypoint(){
+		float waypointDist;
+		float minDist = 10000f; // arbitrarily large
+		GameObject newWP = null;
+		foreach (GameObject o in waypoints) {
+			waypointDist = Vector2.Distance(transform.position, o.transform.position);
+			if (waypointDist < minDist && o.transform != prevWaypoint){
+				minDist = waypointDist;
+				newWP = o;
+			}
+		}
+		curWaypoint = newWP.transform;
+		prevWaypoint = null;
+		return curWaypoint;
+	}
+
+	// detects colision with 'sound' triggers (both player and rock)
+	// sets the target to investigate
 	void OnTriggerEnter2D (Collider2D other) {
 		//if player sound bubble runs into the guard
 		if (other.tag == "Player" || other.tag == "Rock") {
 			pathingTarget = other.transform;
 			currentSpeed = investigateSpeed;
 			waitToPatrol = true;
+			SetNewTarget ();
 		}
 	}
 
+	// makes the guard wait before patrolling again after investigating or chasing something
 	IEnumerator WaitForPeriod(float waitTime) {
 		yield return new WaitForSeconds(waitTime);
-		waitToPatrol = false;
+		// make sure that if the guard picked a new target while waiting, this doesn't activate
+		// otherwise, select the closest waypoint
+		if (path.Count == 0) { 
+			pathingTarget = FindClosestWaypoint();
+			currentSpeed = patrolSpeed;
+			SetNewTarget();
+			waitToPatrol = false;
+		}
 	}
 }
---
+++ Assets/Scripts/WaypointHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaypointHandler : MonoBehaviour {
+
+	public GameObject adjoin1 = null;
+	public GameObject adjoin2 = null;
+	public GameObject adjoin3 = null;
+	public GameObject adjoin4 = null;
+	private List<Transform> adjoining = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+		if (adjoin1 != null) {
+			adjoining.Add (adjoin1.transform);
+		}
+		if (adjoin2 != null) {
+			adjoining.Add (adjoin2.transform);
+		}
+		if (adjoin3 != null) {
+			adjoining.Add (adjoin3.transform);
+		}
+		if (adjoin4 != null) {
+			adjoining.Add (adjoin4.transform);
+		}
+	}
+
+	public List<Transform> RequestAdjoining() {
+		return adjoining;
+	}
+}
