f8e4631b7f6b929b641bcac35bde0059cb88550c
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-05-24 13:54:42 -0500

    Guards patrol and change speeds

--- Assets/Scripts/GuardAI.cs
+++ Assets/Scripts/GuardAI.cs
@@ -4,27 +4,42 @@ using System.Collections.Generic;
 using PigeonCoopToolkit.Navmesh2D;
 
 public class GuardAI : MonoBehaviour {
-	
+
+	// speed and angle
 	public float patrolSpeed;
 	public float investigateSpeed;
 	public float chaseSpeed;
-	public int normalAngle = 20;
-	private int sightAngle;
-	// time for new target
+	private float currentSpeed;
 
+	public int normalAngle = 20;
+	public int cornerAngle = 90;
+	private int sightAngle;
+
+	// target reset
 	private float newTargetTimer;
 	private float distToTarget;
 
-	// pathing variables
+	// pathing
 	private Transform pathingTarget;
 	private List<Vector2> path;
 
+	// sight
 	private RaycastHit2D objectSighted;
 	private int playerMask = (1 << 10) + (1 << 9);
+
+	// patrolling
+	private GameObject[] waypoints;
+	private int numWaypoints;
+	private int curWaypoint;
+
 
 	void Start () {
 		newTargetTimer = 0;
 		sightAngle = normalAngle;
+		waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+		numWaypoints = waypoints.Length;
+		Debug.Log (numWaypoints);
+		currentSpeed = patrolSpeed;
 	}
 
 	// Update is called once per frame
@@ -32,9 +47,16 @@ public class GuardAI : MonoBehaviour {
 		if (newTargetTimer > 0)
 			newTargetTimer--;
 
+		if (path == null || path.Count == 0) {
+			curWaypoint = Random.Range (0, numWaypoints);
+			pathingTarget = waypoints[curWaypoint].transform;
+			currentSpeed = patrolSpeed;
+			SetNewTarget();
+		}
+
 		if(path != null && path.Count != 0)
 		{
-			transform.position = Vector2.MoveTowards(transform.position, path[0], investigateSpeed*Time.deltaTime);
+			transform.position = Vector2.MoveTowards(transform.position, path[0], currentSpeed*Time.deltaTime);
 
 			if(Vector2.Distance(transform.position,path[0]) < 0.01f)
 			{
@@ -46,7 +68,9 @@ public class GuardAI : MonoBehaviour {
 				Quaternion rotation = Quaternion.LookRotation
 					(path3D - transform.position, transform.TransformDirection(Vector3.forward));
 				transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-				sightAngle = 90;
+			}
+			else {
+				sightAngle = cornerAngle; // Increasing sight angle for 1 frame to look around corners when target is lost
 			}
 		}
 
@@ -54,8 +78,10 @@ public class GuardAI : MonoBehaviour {
 			Vector2 dir = Quaternion.AngleAxis(i, Vector3.forward) * -transform.up;
 			objectSighted = Physics2D.Raycast (transform.position, dir, 50f, playerMask);
 			if (objectSighted) {
-				if (objectSighted.transform.tag == "Player")
+				if (objectSighted.transform.tag == "Player") {
 					pathingTarget = objectSighted.transform;
+					currentSpeed = chaseSpeed;
+				}
 			}
 		}
 		sightAngle = normalAngle;
@@ -65,7 +91,8 @@ public class GuardAI : MonoBehaviour {
 	void SetNewTarget() {
 		if ( pathingTarget != null && newTargetTimer == 0) {
 			path = NavMesh2D.GetSmoothedPath (transform.position, pathingTarget.position);
-			
+
+			// set timer to reset target relative to distance from target, for efficiency
 			distToTarget = Vector2.Distance (transform.position, pathingTarget.position);
 			if (distToTarget < 20)
 				newTargetTimer = 10;
@@ -82,6 +109,7 @@ public class GuardAI : MonoBehaviour {
 		//if player sound bubble runs into the guard
 		if (other.tag == "Player" || other.tag == "Rock") {
 			pathingTarget = other.transform;
+			currentSpeed = investigateSpeed;
 		}
 	}
 }
