573864dee48c76932593bb1ee45c23038c0689ba
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-05-26 12:23:03 -0500

    saved script...

--- Assets/Scripts/GuardMove.cs
+++ Assets/Scripts/GuardMove.cs
@@ -7,10 +7,6 @@ public class GuardMove : MonoBehaviour {
 	public GameObject guardLight;
 	private GameObject myLight;
 
-	private PlayerStats ps;
-	private GameObject[] spawnPoints;
-	private List<GameObject> spawnOptions;
-
 	// Use this for initialization
 	void Start () {
 		myLight = Instantiate (guardLight);
@@ -19,23 +15,5 @@ public class GuardMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		myLight.transform.position = transform.position;
-	}
-
-	void OnCollisionEnter2D(Collision2D other) {
-		if (other.gameObject.tag == "Player" && !other.collider.isTrigger) {
-			ps = other.gameObject.GetComponent<PlayerStats>();
-			ps.lootTotal /= 2;
-
-			spawnPoints = GameObject.FindGameObjectsWithTag("spawn");
-			spawnOptions = new List<GameObject> ();
-			for (int i = 0; i < spawnPoints.Length; i++) {
-				spawnOptions.Add (spawnPoints [i]);
-			}
-
-			int r = Random.Range (0, spawnOptions.Count);
-			GameObject mySpawnPoint = spawnOptions [r];
-
-			ps.transform.position = mySpawnPoint.transform.position;
-		}
 	}
 }
