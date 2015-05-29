b7c1060c616c8033a01bfec3799fd5b58f3985bd
Richard Herndon <richardmherndon@gmail.com>
2015-05-29 12:54:32 -0500

    proper loot penalty upon being caught

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -109,7 +109,9 @@ public class Move : MonoBehaviour{
 	//check if guard runs into us
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.tag == "guard") {
-			gameObject.GetComponent<PlayerStats>().lootTotal /= 2;
+			float l = Mathf.Floor (GetComponent<PlayerStats>().lootTotal*0.1f);
+			int il = (int) l;
+			gameObject.GetComponent<PlayerStats>().AddLoot (-1*il);
 			
 			int r = Random.Range (0, spawnPoints.Length);
 			GameObject mySpawnPoint = spawnPoints [r];
