730d8bb1119f27a46c743f59d5f3ee4021770ac5
kmads <kamaddio@gmail.com>
2015-05-29 15:11:56 -0500

    Basic Start Screen

---
+++ Assets/Scripts/StartScreen/StartScreenScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	public Button startText;
+
+	// Use this for initialization
+	void Start () {
+		startText = startText.GetComponent<Button> ();
+	}
+	
+	public void StartLevel(){
+		PhotonNetwork.automaticallySyncScene = true;
+		PhotonNetwork.LoadLevel (1); // this currently loads AudioTest
+	}
+}
