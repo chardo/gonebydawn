432565c5c33201998d4b367367d519fe02a7671b
Richard Herndon <richardmherndon@gmail.com>
2015-05-29 00:15:43 -0500

    start message

---
+++ Assets/Scripts/UI/MessageDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MessageDisplay : MonoBehaviour {
+
+	Text messageText;
+
+	// Use this for initialization
+	void Start () {
+		messageText = GetComponent<Text> ();
+		messageText.text = "Steal what you can and get out by 6am!";
+		StartCoroutine (setBlankAfter (5));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator setBlankAfter(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		messageText.text = "";
+	}
+}
