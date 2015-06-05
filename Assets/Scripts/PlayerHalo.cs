5cf4a9f5738b3ef01e150316b8f9b9349dfb5683
MichaelJH <michaelhorst2016@u.northwestern.edu>
2015-06-04 19:30:27 -0500

    fixed halos

--- Assets/Scripts/PlayerHalo.cs
+++ Assets/Scripts/PlayerHalo.cs
@@ -3,22 +3,11 @@ using System.Collections;
 
 public class PlayerHalo : Photon.MonoBehaviour {
 	
-	private Object myHalo;
+	public Object myHalo;
 	private GameObject halo;
 	private int ID;
 
 	void Start () {
-		if (photonView.isMine) {
-			ID = PhotonNetwork.player.ID;
-		}
-		if (ID == 1)
-			myHalo = Instantiate(Resources.Load ("Player1Halo"));
-		else if (ID == 2)
-			myHalo = Instantiate(Resources.Load ("Player2Halo"));
-		else if (ID == 3)
-			myHalo = Instantiate(Resources.Load ("Player3Halo"));
-		else
-			myHalo = Instantiate(Resources.Load ("Player1Halo"));
 		halo = myHalo as GameObject;
 	}
 	
