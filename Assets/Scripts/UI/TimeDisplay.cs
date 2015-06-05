a82f2fb380a136ef88e6492fbf743338803345d9
kmads <kamaddio@gmail.com>
2015-06-05 11:14:19 -0500

    Wait to start timer until 3 players are joined

--- Assets/Scripts/UI/TimeDisplay.cs
+++ Assets/Scripts/UI/TimeDisplay.cs
@@ -16,16 +16,32 @@ public class TimeDisplay : MonoBehaviour {
 	public GameObject message;
 	Text warningText;
 
+	public bool startTimer = false;
+
 	// Use this for initialization
 	void Start () {
 		timeText = GetComponent<Text> ();
 		timeText.text = timeCounter.ToString ("H:mm") + " a.m.";
 		warningText = message.GetComponent<Text> ();
 	}
+
+	void OnJoinedRoom(){
+		if (PhotonNetwork.room.playerCount == 3) {
+			startTimer = true;
+		}
+	}
+
+	void OnPhotonPlayerConnected(PhotonPlayer newPlayer){
+		if (PhotonNetwork.room.playerCount == 3) {
+			startTimer = true;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		timeCounter = timeCounter.AddSeconds (1);
+		if (startTimer == true) {
+			timeCounter = timeCounter.AddSeconds (1);
+		}
 		timeText.text = timeCounter.ToString ("H:mm") + " a.m.";
 		if (!haveBeenWarned && timeCounter >= warningTime) {
 			haveBeenWarned = true;
